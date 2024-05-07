6fab5074adac28766e1e977e634363c081b322c2
ChingHsun <carol331536@gmail.com>
2024-05-07 10:26:11 +0800

    feat: remove change months actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
-import { useEffect, useState } from "react";
-import { format, addMonths } from "date-fns";
+import { format } from "date-fns";
 import "./App.css";
 import Day from "./components/Day";
 import getDays from "./utils/getDays";
 
 function App() {
-  const [currentDateState, setCurrentDateState] = useState<Date>(new Date());
-
-  const [days, setDays] = useState<Date[]>([]);
-
-  const goToPreviousMonth = () => {
-    setCurrentDateState((prevDate) => addMonths(prevDate, -1));
-  };
-
-  const goToNextMonth = () => {
-    setCurrentDateState((prevDate) => addMonths(prevDate, 1));
-  };
-
-  useEffect(() => {
-    setDays(getDays(currentDateState));
-  }, [currentDateState]);
+  const currentDate = new Date();
+  const days = getDays(currentDate);
 
   return (
     <div className="layout">
       <div className="header">
-        <button onClick={goToPreviousMonth}>&#8249;</button>
-        <div>{format(currentDateState, "yyyy 年 M 月")}</div>
-        <button onClick={goToNextMonth}>&#8250;</button>
+        <button>&#8249;</button>
+        <div>{format(currentDate, "yyyy 年 M 月")}</div>
+        <button>&#8250;</button>
       </div>
       <div className="calendar">
         {days.map((day) => (
-          <Day key={day.toString()} date={day} currentDate={currentDateState} />
+          <Day key={day.toString()} date={day} currentDate={currentDate} />
         ))}
       </div>
     </div>
